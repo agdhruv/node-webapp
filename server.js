7e9de76e6b19029ba9e3522b0eb035cd5a6c2052
agdhruv <dhruv_agarwal@live.com>
2017-09-11 03:54:22 +0530

    HANDLEBARS IS AMAZING

--- server.js
+++ server.js
@@ -3,24 +3,31 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+// tell the app that we will be using handlebars for our templating
 app.set('view engine', 'hbs');
+// we can register hbs helpers to call from the hbs template
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
 
 // Make static web server to avoid creating routes for every static file
 app.use(express.static(__dirname + '/public')); // use method helps use Express Middleware like express.static()
 
 app.get('/', (req, res) => {
-	// res.send('<h2>Hello Express!</h2>');
-	res.send({
-		name: 'Andrew',
-		likes: [
-			'Tennis',
-			'Badminton'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my website'
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.render('about.hbs');
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
 });
 
 app.get('/bad', (req, res) => {
