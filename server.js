bb36a62e12722ab120da9e45f71c785c1e54b693
agdhruv <dhruv_agarwal@live.com>
2017-09-13 20:35:10 +0530

    Templating and middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 // tell the app that we will be using handlebars for our templating
 app.set('view engine', 'hbs');
+
 // we can register hbs helpers to call from the hbs template
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear();
@@ -16,6 +18,21 @@ hbs.registerHelper('screamIt', (text) => {
 
 // Make static web server to avoid creating routes for every static file
 app.use(express.static(__dirname + '/public')); // use method helps use Express Middleware like express.static()
+
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err){
+			console.log('Unable to append to server.log.');
+		}
+	});
+	next();
+});
+
 
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
